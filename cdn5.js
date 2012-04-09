9a20d82edf629f2280eea9b34951d0bf936ed600
Josh Cody <joshkcody@gmail.com>
2012-04-09 12:00:21 -0700

    Added initial test objects pulled from website repository

---
+++ cdn5.js
@@ -0,0 +1,2 @@
+<!-- Copyright 2009-2010 Cedexis Inc. -->
+var x = true;
---
+++ cdx9.js
@@ -0,0 +1,6 @@
+<!-- Copyright 2010 Cedexis Inc. -->
+var s = new Date;
+
+<!-- FILLER xxxxxxx -->
+
+if ('object' === typeof window.radar) { window.radar.stoppedAt = s; }
