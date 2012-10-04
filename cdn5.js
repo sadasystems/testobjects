ddf3190ffd79ce05d1a591c1b2fbb00f1491ce87
Marty Kagan <marty@cedexis.com>
2012-10-03 20:55:56 -0700

    new new test objects

--- cdn5.js
+++
@@ -1,2 +0,0 @@
-<!-- Copyright 2009-2010 Cedexis Inc. -->
-var x = true;
--- cdx9.js
+++
@@ -1,6 +0,0 @@
-<!-- Copyright 2010 Cedexis Inc. -->
-var s = new Date;
-
-<!-- FILLER xxxxxxx -->
-
-if ('object' === typeof window.radar) { window.radar.stoppedAt = s; }
